e4f96fd6cf954c931b116d16eb6d34de57437cba
mark <markichnich@proton.me>
2022-11-25 21:22:29 +0100

    add render function for entries.

--- src/entry.rs
+++ src/entry.rs
@@ -30,6 +30,40 @@ impl<'a> Entry<'a> {
             done: false,
         }
     }
+
+    pub fn render(&self, width: i32) -> String {
+        let mut available_space = width;
+        let mut label: String;
+
+        available_space -= " - [ ] ".len() as i32;
+        if self.due.is_some() {
+            available_space -= " [yyyy-mm-dd, HH:MM] ".len() as i32;
+        } else {
+            available_space -= " ".len() as i32;
+        }
+
+        if available_space < self.label.len() as i32 {
+            available_space -= "...".len() as i32;
+            label = self.label.chars().take(available_space as usize).collect();
+            label.push_str("...");
+        } else {
+            label = self.label.to_string();
+            let padding = available_space as usize - label.len();
+            let padding = " ".repeat(padding);
+            label.push_str(&padding);
+        }
+
+        let mut text: String = if self.done { " - [x] " } else { " - [ ] " }.to_string();
+        text.push_str(&label);
+
+        if self.due.is_some() {
+            text.push_str(" [");
+            text.push_str(&format!("{}", self.due.as_ref().unwrap()));
+            text.push_str("] ");
+        }
+
+        text
+    }
 }
 
 impl Date {
