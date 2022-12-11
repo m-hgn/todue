e9d558a08c82aa7e955f1dc1b92ae1725fd21fc3
mark <markichnich@proton.me>
2022-12-11 18:18:25 +0100

    get started with clap.

--- src/args.rs
+++ src/args.rs
@@ -1,13 +1,18 @@
-use clap:: {
-    Args,
-    Parser,
-    Subcommand
-};
+use clap::Parser;
 
 #[derive(Debug, Parser)]
-#[clap(author, version, about)]
-pub struct TodueArgs {
+#[command(author, version, about)]
+pub struct Args {
+    /// The markdown file to use
+    #[clap(default_value_t = String::from("TODO.md"))]
+    file: String,
 
-    ///
+    /// Whether to ignore deadlines
+    #[arg(long)]
+    ignore_deadlines: bool,
+
+    /// Whether to ignore priorities
+    #[arg(long)]
+    ignore_priorities: bool,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate clap;
+
+mod args;
+
+use args::Args;
+use clap::Parser;
 
 fn main() {
+    let args = Args::parse();
+
 }
